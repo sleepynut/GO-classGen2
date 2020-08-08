269d004f10d2a89206a9c88cc37e68105d08a358
Nut Pawasuthiwong <nut.pa@kbtg.tech>
2020-08-08 14:11:03 +0700

    working on something

--- slice/main.go
+++ slice/main.go
@@ -3,9 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	primes := [6]int{2, 3, 5, 7, 11, 13}
+	s := []int{2, 3, 5, 7, 11, 13}
 
-	s := primes[0:6]
-	fmt.Printf("%T\n", primes)
-	fmt.Printf("%#v\n", s)
+	s = s[1:4]
+	fmt.Println(s)
+
+	s = s[:2]
+	fmt.Println(s)
+
+	s = s[1:]
+	fmt.Println(s)
 }
