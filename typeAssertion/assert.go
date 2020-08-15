2f9233b0e10b1f18c7cc5e143231fdcf94906b36
Nut Pawasuthiwong <nut.pa@kbtg.tech>
2020-08-15 13:55:54 +0700

    switch type assertion

--- typeAssertion/assert.go
+++ typeAssertion/assert.go
@@ -6,7 +6,16 @@ func main() {
 	var i interface{} = "hello"
 	s := i.(string)
 	fmt.Println(s)
-	if s, ok := i.(string); ok {
-		fmt.Println(s)
+	switch i.(type) {
+	case nil:
+		fmt.Printf("%T\n", i)
+	case int:
+		fmt.Printf("%T\n", i)
+	case string:
+		fmt.Printf("%T\n", i)
+	case []int:
+		fmt.Printf("%T\n", i)
+	default:
+		fmt.Printf("%T\n", i)
 	}
 }
