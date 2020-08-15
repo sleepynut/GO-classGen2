53e77c6985bebce9f3a6f57c39b2ef7008906196
Nut Pawasuthiwong <nut.pa@kbtg.tech>
2020-08-15 14:42:02 +0700

    Sample commit via VIM

--- typeAssertion/assert.go
+++ typeAssertion/assert.go
@@ -2,19 +2,21 @@ package main
 
 import "fmt"
 
+type Samsung struct {
+	version string
+}
+
 func main() {
-	var i interface{} = "hello"
-	s := i.(string)
-	fmt.Println(s)
+	var i interface{} = []string{"MY", "GOD"}
+	i = &Samsung{version: "s10+"}
+	// s := i.(string)
+	// fmt.Println(s)
 	switch i.(type) {
-	case nil:
+	case nil, int, string, []int:
 		fmt.Printf("%T\n", i)
-	case int:
-		fmt.Printf("%T\n", i)
-	case string:
-		fmt.Printf("%T\n", i)
-	case []int:
-		fmt.Printf("%T\n", i)
+	case *Samsung, Samsung:
+		p, _ := i.(*Samsung)
+		fmt.Printf("Version of my phone %s\n", p.version)
 	default:
 		fmt.Printf("%T\n", i)
 	}
