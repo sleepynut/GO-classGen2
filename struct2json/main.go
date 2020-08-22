765d56483a564fe53b891e7298f95d178939bf37
Nut Pawasuthiwong <nut.pa@kbtg.tech>
2020-08-22 15:52:51 +0700

    json http lesson

---
+++ struct2json/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+)
+
+type Todo struct {
+	ID     int       `json:"id"`
+	Title  string    `json:"title"`
+	Status string    `json:"status"`
+	Time   time.Time `json:"time"`
+}
+
+func main() {
+	t := Todo{
+		ID:     1,
+		Title:  "pay credit card",
+		Status: "completed",
+		Time:   time.Now(),
+	}
+	b, err := json.Marshal(t)
+	fmt.Printf("type : %T \n", b)
+	fmt.Printf("byte : %v \n", b)
+	fmt.Printf("string: %s \n", b)
+	fmt.Println(err)
+}
