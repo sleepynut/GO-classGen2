9d2ae2d2f194cb78d3b12557485fb3209055c281
Nut Pawasuthiwong <nut.pa@kbtg.tech>
2020-08-26 16:47:18 +0700

    myAPI with more rest Functions

--- myAPI/go.mod
+++ myAPI/go.mod
@@ -1,6 +1,6 @@
 module github.com/sleepynut/Go-classGen2-1/myAPI
 
-go 1.14
+go 1.15
 
 require (
 	github.com/gin-gonic/gin v1.6.3
--- myAPI/main.go
+++ myAPI/main.go
@@ -2,28 +2,10 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
-
-func helloHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "hello",
-	})
-}
-
-func getTodosHandler(c *gin.Context) {
-	// c.JSON(http.StatusOK, gin.H{
-	// 	"id":     1,
-	// 	"title":  "pay phone bills",
-	// 	"status": "active",
-	// })
-	var items []*Todo
-	for _, i := range todos {
-		items = append(items, i)
-	}
-	c.JSON(http.StatusOK, items)
-}
 
 type Todo struct {
 	ID     int    `json:"id"`
@@ -32,7 +14,98 @@ type Todo struct {
 }
 
 var todos = map[int]*Todo{
-	1: &Todo{ID: 1, Title: "pay phone bills", Status: "Active"},
+	1: &Todo{ID: 1, Title: "pay phone bills", Status: "active"},
+}
+
+func helloHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "hello",
+	})
+}
+
+func getTodosHandler(c *gin.Context) {
+	var items []*Todo
+	stat := c.Query("status")
+
+	for _, i := range todos {
+		if stat == "" || stat == i.Status {
+			items = append(items, i)
+		}
+	}
+	c.JSON(http.StatusOK, items)
+}
+
+func getTodosByIDHandler(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	todo, ok := todos[id]
+	if !ok {
+		// DO NOT return NIL in restAPI --> not recommended
+		c.JSON(http.StatusOK, gin.H{})
+		return
+	}
+
+	c.JSON(http.StatusOK, todo)
+}
+
+// func getTodosByFilterHandler(c *gin.Context) {
+// 	var items []*Todo
+// 	stat := c.Query("status")
+// 	for _, v := range todos {
+// 		if stat != "" && v.Status == stat {
+// 			items = append(items, v)
+// 		}
+// 	}
+// 	c.JSON(http.StatusOK, items)
+// }
+
+func postTodosHandler(c *gin.Context) {
+	t := Todo{}
+	if err := c.ShouldBindJSON(&t); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	id := len(todos)
+	id++
+
+	t.ID = id
+	todos[id] = &t
+
+	c.JSON(http.StatusCreated, "created todo.")
+
+}
+
+func updateTodosHandler(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	t := Todo{}
+	if err = c.ShouldBindJSON(&t); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	t.ID = id
+	todos[id] = &t
+	c.JSON(http.StatusOK, t)
+}
+
+func deleteTodosHandler(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	delete(todos, id)
+	c.JSON(http.StatusOK, "deleted todo")
 }
 
 func main() {
@@ -41,5 +114,9 @@ func main() {
 	// r.Run() // listen and serve on 127.0.0.0:8080 <- default port: 8080
 
 	r.GET("/todos", getTodosHandler)
+	r.GET("/todos/:id", getTodosByIDHandler)
+	r.POST("/todos", postTodosHandler)
+	r.PUT("/todos/:id", updateTodosHandler)
+	r.DELETE("/todos/:id", deleteTodosHandler)
 	r.Run(":1234")
 }
