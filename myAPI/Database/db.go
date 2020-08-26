295e7c7c532a78e2127d500694d92bcb06bac9e3
Nut Pawasuthiwong <nut.pa@kbtg.tech>
2020-08-26 23:17:01 +0700

    add update,queryall

--- myAPI/Database/db.go
+++ myAPI/Database/db.go
@@ -9,44 +9,14 @@ import (
 	_ "github.com/lib/pq"
 )
 
-// func main() {
-// 	url := os.Getenv("DATABASE_URL")
-// 	// url := "postgres://peoqxscq:o8KzOLhBc8U2tOjVkXN3g2Aj4iVSARXq@satao.db.elephantsql.com:5432/peoqxscq"
-// 	db, err := sql.Open("postgres", url)
-
-// 	if err != nil {
-// 		log.Fatal(err)
-// 		return
-// 	}
-// 	defer db.Close()
-
-// 	createTb := `
-// 	CREATE TABLE IF NOT EXISTS todos (
-// 		id SERIAL PRIMARY KEY,
-// 		title TEXT,
-// 		status TEXT
-// 	);
-// 	`
-
-// 	// selectTb := `
-// 	// SELECT *
-// 	// from todos
-// 	// `
-
-// 	_, err = db.Exec(createTb)
-
-// 	if err != nil {
-// 		log.Fatal("can't create table", err)
-// 	}
-
-// 	log.Println("create table success")
-// }
-
-func createTable() {
-	url := os.Getenv("DATABASE_URL")
+func openConnection() (db *sql.DB, err error) {
 	// url := "postgres://peoqxscq:o8KzOLhBc8U2tOjVkXN3g2Aj4iVSARXq@satao.db.elephantsql.com:5432/peoqxscq"
-	db, err := sql.Open("postgres", url)
-
+	url := os.Getenv("DATABASE_URL")
+	db, err = sql.Open("postgres", url)
+	return
+}
+func createTable() {
+	db, err := openConnection()
 	if err != nil {
 		log.Fatal(err)
 		return
@@ -60,6 +30,12 @@ func createTable() {
 		status TEXT
 	);
 	`
+
+	// db.Exec means to execute the given SQL
+	// w/o any result|state from the result
+	// following the execution
+	// however the PLACEHOLDER is used to track
+	// number of rows AFFECTED from the execution
 	_, err = db.Exec(createTb)
 
 	if err != nil {
@@ -70,14 +46,14 @@ func createTable() {
 }
 
 func insertTodo() {
-	// url := "postgres://peoqxscq:o8KzOLhBc8U2tOjVkXN3g2Aj4iVSARXq@satao.db.elephantsql.com:5432/peoqxscq"
-	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
-	// db, err := sql.Open("postgres", url)
+	db, err := openConnection()
 	if err != nil {
-		log.Fatal("Connect to database error", err)
+		log.Fatal(err)
+		return
 	}
 	defer db.Close()
-	row := db.QueryRow("INSERT INTO todos (title, status) values ($1, $2)  RETURNING id", "buy bmw", "active")
+
+	row := db.QueryRow("INSERT INTO todos (title, status) values ($1, $2)  RETURNING id;", "talk with my ex", "never")
 	var id int
 	err = row.Scan(&id)
 	if err != nil {
@@ -88,23 +64,27 @@ func insertTodo() {
 }
 
 func queryOneRow() {
-	// url := "postgres://peoqxscq:o8KzOLhBc8U2tOjVkXN3g2Aj4iVSARXq@satao.db.elephantsql.com:5432/peoqxscq"
-	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
-	// db, err := sql.Open("postgres", url)
+	db, err := openConnection()
 	if err != nil {
-		log.Fatal("Connect to database error", err)
+		log.Fatal(err)
+		return
 	}
 	defer db.Close()
-	stmt, err := db.Prepare("SELECT id, title, status FROM todos where id=$1")
+
+	stmt, err := db.Prepare("SELECT id, title, status FROM todos where id=$1;")
 	if err != nil {
-		log.Fatal("can'tprepare query one row statment", err)
+		log.Fatal("can't prepare query one row statment", err)
 	}
 
 	rowId := 1
+
+	// rowId corresponds to $variable in db.Prepare
+	// can use multiple variables by adding more $variable
 	row := stmt.QueryRow(rowId)
 	var id int
 	var title, status string
 
+	// store results from stmt.QueryRow to each variable
 	err = row.Scan(&id, &title, &status)
 	if err != nil {
 		log.Fatal("can't Scan row into variables", err)
@@ -112,13 +92,58 @@ func queryOneRow() {
 	fmt.Println("one row", id, title, status)
 }
 
+func updateTodo() {
+	db, err := openConnection()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer db.Close()
+
+	stmt, err := db.Prepare("update todos set status=$1 where id=$2;")
+	if err != nil {
+		log.Fatal("can't prepare update statement", err)
+	}
+
+	if _, err := stmt.Exec("complete", 3); err != nil {
+		log.Fatal("can't update", err)
+	}
+	fmt.Println("update success")
+}
+
+func queryAllTodo() {
+	db, err := openConnection()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer db.Close()
+
+	stmt, err := db.Prepare("select * from todos")
+	if err != nil {
+		log.Fatal("can't prepare query all todos statement", err)
+	}
+
+	rows, err := stmt.Query()
+	if err != nil {
+		log.Fatal("can't query all todos", err)
+	}
+
+	var id int
+	var title, status string
+	for rows.Next() {
+		if err := rows.Scan(&id, &title, &status); err != nil {
+			log.Fatal("can't scane row into id,title,status", err)
+			continue
+		}
+		fmt.Println(id, title, status)
+	}
+	fmt.Println("query all todos success")
+}
+
 func main() {
-	// insertTodo()
-
-	// os.Setenv("DATABASE_URL", "postgres://peoqxscq:o8KzOLhBc8U2tOjVkXN3g2Aj4iVSARXq@satao.db.elephantsql.com:5432/peoqxscq")
-	// fmt.Println(os.Getenv("DATABASE_URL"))
-
 	// queryOneRow()
-
-	createTable()
+	// insertTodo()
+	// updateTodo()
+	queryAllTodo()
 }
