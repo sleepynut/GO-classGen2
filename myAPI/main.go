611e11c6d1e1b8627db504015e0bbca6e9d5beed
Nut Pawasuthiwong <nut.pa@kbtg.tech>
2020-08-26 14:55:20 +0700

    gin example

--- myAPI/main.go
+++ myAPI/main.go
@@ -13,13 +13,33 @@ func helloHandler(c *gin.Context) {
 }
 
 func getTodosHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "hello Gin GET todos",
-	})
+	// c.JSON(http.StatusOK, gin.H{
+	// 	"id":     1,
+	// 	"title":  "pay phone bills",
+	// 	"status": "active",
+	// })
+	var items []*Todo
+	for _, i := range todos {
+		items = append(items, i)
+	}
+	c.JSON(http.StatusOK, items)
+}
+
+type Todo struct {
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Status string `json:"status"`
+}
+
+var todos = map[int]*Todo{
+	1: &Todo{ID: 1, Title: "pay phone bills", Status: "Active"},
 }
 
 func main() {
 	r := gin.Default()
+	// r.GET("/hello", helloHandler)
+	// r.Run() // listen and serve on 127.0.0.0:8080 <- default port: 8080
+
 	r.GET("/todos", getTodosHandler)
-	r.Run(":1234") // listen and serve on 127.0.0.0:8080
+	r.Run(":1234")
 }
