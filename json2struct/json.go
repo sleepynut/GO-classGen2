7362839191e08f9a56aaa20acdce02d833abe036
Nut Pawasuthiwong <nut.pa@kbtg.tech>
2020-08-26 11:24:04 +0700

    marshaller example

--- json2struct/json.go
+++ json2struct/json.go
@@ -3,19 +3,39 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
+	"time"
 )
 
+type Birthday time.Time
+
+func (bd *Birthday) UnmarshalJSON(b []byte) error {
+	fmt.Println("Here")
+	s, err := time.Parse("2006-01-02", strings.Trim(string(b), "\""))
+	if err != nil {
+		return err
+	}
+
+	*bd = Birthday(s)
+	return nil
+}
+func (bd Birthday) MarshalJSON() ([]byte, error) {
+	return json.Marshal(bd)
+}
+
 type Todo struct {
-	ID     int    `json:"id"`
-	Title  string `json:"title"`
-	Status string `json:"status"`
+	ID     int      `json:"id"`
+	Title  string   `json:"title"`
+	Status string   `json:"status"`
+	Time   Birthday `json:"time"`
 }
 
 func main() {
 	data := []byte(`{
 				"id": 1,
 				"title": "pay credit card",
-				"status": "active"
+				"status": "active",
+				"time":"2020-08-26"
 			}`)
 	var t Todo
 	err := json.Unmarshal(data, &t)
@@ -24,6 +44,11 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	fmt.Printf("% #v\n", t)
+	fmt.Printf("%v\n", t.Time)
+	fmt.Println("=================")
+	{
+		s, _ := time.Parse("2006-01-02", "2020-08-26")
+		fmt.Println("HERE>", s)
+	}
 
 }
